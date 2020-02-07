4bdf9bfbddd0ddfa2261493a7a85cbd2979e693a
Andrey <cy6ernut@gmail.com>
2020-02-07 12:12:59 +0300

    Заглушка для пустого списка

--- app/src/main/java/ru/cybernut/agreement/adapters/RequestAdapter.kt
+++ app/src/main/java/ru/cybernut/agreement/adapters/RequestAdapter.kt
@@ -8,12 +8,15 @@ import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import ru.cybernut.agreement.R
 import ru.cybernut.agreement.data.Request
 import java.util.*
 
 class RequestsAdapter(@LayoutRes val itemLayoutId: Int, val bindingVariableId: Int, val onClickListener: OnClickListener? = null): ListAdapter<Request, RequestsAdapter.RequestViewHolder>(DiffCallback) {
 
     private val additionalBindingVariables = Hashtable<Int, Any>()
+
+    private var isEmptyList: Boolean = false
 
     companion object DiffCallback : DiffUtil.ItemCallback<Request>() {
         override fun areItemsTheSame(oldItem: Request, newItem: Request): Boolean {
@@ -26,26 +29,58 @@ class RequestsAdapter(@LayoutRes val itemLayoutId: Int, val bindingVariableId: I
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RequestViewHolder {
-        return RequestViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), itemLayoutId, parent, false), bindingVariableId)
+        if (viewType == -1) {
+            return RequestViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.empty_request_list_item, parent, false), null)
+        } else {
+            return RequestViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    itemLayoutId,
+                    parent,
+                    false
+                ), bindingVariableId
+            )
+        }
     }
 
     override fun onBindViewHolder(holder: RequestViewHolder, position: Int) {
-        val request = getItem(position)
-        if (onClickListener != null) {
-            holder.itemView.setOnClickListener{ onClickListener.onClick(request) }
+        if( holder.itemViewType == -1) {
+            return
+        } else {
+            val request = getItem(position)
+            if (onClickListener != null) {
+                holder.itemView.setOnClickListener { onClickListener.onClick(request) }
+            }
+            holder.bind(request)
         }
-        holder.bind(request)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (!isEmptyList) 0 else -1
+    }
+
+    override fun getItemCount(): Int {
+        val size = super.getItemCount()
+        if (size == 0) {
+            isEmptyList = true
+            return 1
+        } else {
+            isEmptyList = false
+            return size
+        }
     }
 
     fun addBindingVariable(bindingId: Int, value: Any) {
         additionalBindingVariables[bindingId] = value
     }
 
-    inner class RequestViewHolder(private var binding: ViewDataBinding, val bindingVariableId: Int): RecyclerView.ViewHolder(binding.root) {
+    inner class RequestViewHolder(private var binding: ViewDataBinding, val bindingVariableId: Int?): RecyclerView.ViewHolder(binding.root) {
         fun<T> bind(variable: T) {
-            binding.setVariable(bindingVariableId, variable)
-            additionalBindingVariables.forEach{ binding.setVariable(it.key, it.value) }
-            binding.executePendingBindings()
+            if (bindingVariableId!= null) {
+                binding.setVariable(bindingVariableId, variable)
+                additionalBindingVariables.forEach { binding.setVariable(it.key, it.value) }
+                binding.executePendingBindings()
+            }
         }
     }
 
